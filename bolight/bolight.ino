40dde89c28e6cf0e688cfd6bb72b870751b9b1e5
Magform <nicolas25.02.2003.nf@gmail.com>
2022-02-07 15:45:40 +0100

    Release
    added comments, removed usless things

--- bolight/bolight.ino
+++ bolight/bolight.ino
@@ -4,14 +4,15 @@
 
 
 //Variable that you need to change
-#define WIFI_SSID "Interno12"   // replace Your_SSID with your wifi SSID
-#define WIFI_PASS "Magnolia88."  // replace Your_Password with your wifi password
-#define IFTTT_Key "MJJT3l04HEftIE8EBgDOhXZm-JDlmarkxQwoXT8fgY" // replace Your_Webhooks with your IFTTT Webhooks key
-#define IFTTT_EventName "bottone"
+#define WIFI_SSID "Your_SSID"   // replace Your_SSID with your wifi SSID
+#define WIFI_PASS "Your_Password"  // replace Your_Password with your wifi password
+#define IFTTT_Key "Your_Webhooks" // replace Your_Webhooks with your IFTTT Webhooks key
+#define IFTTT_EventName "Your_Event" // replace Your_Event with your IFTTT event name
 
+//Connetion
 const int button = 13;
-int state=0;
 
+//function that see if the button is pressed
 boolean setstate(){
    int currentState = digitalRead(button);
    if(currentState==1){
@@ -21,31 +22,32 @@ boolean setstate(){
    }
 
 void setup() {
-   Serial.begin(115200);
-   Serial.println();
+  
+  Serial.begin(115200);
   Serial.println("");
-     // Connecting to WiFi...
   WiFi.begin(WIFI_SSID, WIFI_PASS); 
-  Serial.print("Connecting to ");
+  Serial.print("Connecting to ");  // Connecting to WiFi
   Serial.print(WIFI_SSID);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(100);
-    Serial.print(".");
+    Serial.print("."); 
 
   }
- 
-  // Connected to WiFi
   Serial.println();
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
+
+  //set button as input
   pinMode(button, INPUT_PULLUP);
 
 }
 
 void loop() {
+  
   if(setstate()){
     Serial.println("Button pressed");
     send_webhook(IFTTT_EventName,IFTTT_Key,"" ,"" , "");
     }
+    
 }
